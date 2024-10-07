4fc2d237a0c37eaa9487d9214812144fe9460e79
Kalpesh Gunjal <89968295+Kalpeshgunjal07@users.noreply.github.com>
2024-10-07 07:26:11 +0000

    push

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,51 @@
+let score = true
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+//"33" => 33
+//"3abv"=> NaN
+// true => 1; false=>0
+//""=> false
+//"kalpesh"=> true
+// let isLoggedIn=1
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+// let sumNumber= 33;
+// let stringNumber = String(sumNumber);
+// console.log(typeof stringNumber);
+
+
+//*************#**********************Operations*************************8 */
+// let value =3
+// let negValue = -value
+// // console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "hello"
+// let str2= "kalpesh"
+
+// let str = str1 + str2;
+// console.log(str);
+// console.log("1"+"2");
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+// let num1, num2,num3
+// num1 = num2 = num3 = 2+2
+let gamecounter = 100
+
+++gamecounter;
+console.log(gamecounter);
